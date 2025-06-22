98912864f01ed5bb983e8a59af92a71990f28ed6
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 18:23:46 +0530

    Create Main.java

---
+++ Week1_Algorithms_Data Structures/Exercise 7: Financial Forecasting/Main.java
@@ -0,0 +1,17 @@
+package com.forecast;
+import java.util.Arrays;
+public class Main {
+
+	public static void main(String[] args) {
+		 double[] sales = {50000, 55000, 60500}; 
+	        double growthRate = 0.10; 
+
+	        FinancialForecast forecast = new FinancialForecast(sales, growthRate);
+
+	        double nextYear = forecast.forecastNextYear();
+	        System.out.println("Forecast for next year: $" + nextYear);
+
+	        double[] next5Years = forecast.forecastNextNYears(5);
+	        System.out.println("Forecast for next 5 years: " + Arrays.toString(next5Years));
+	    }
+	
