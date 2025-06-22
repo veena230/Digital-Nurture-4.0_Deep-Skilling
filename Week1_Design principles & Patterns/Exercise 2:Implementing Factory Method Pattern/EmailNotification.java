935ad0a137159f18324ab8eaa5b62f2f45896a35
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 17:50:16 +0530

    Create EmailNotification.java

---
+++ Week1_Design principles & Patterns/Exercise 2:Implementing Factory Method Pattern/EmailNotification.java
@@ -0,0 +1,7 @@
+package com.notification;
+public class EmailNotification implements Notification {
+    @Override
+    public void notifyUser() {
+        System.out.println("Sending an Email Notification");
+    }
+}
