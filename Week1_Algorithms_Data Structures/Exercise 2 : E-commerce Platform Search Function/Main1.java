b42a1db2f69bfbbe407ba53d349912a805f7a9f8
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 18:31:31 +0530

    Create Main1.java

---
+++ Week1_Algorithms_Data Structures/Exercise 2 : E-commerce Platform Search Function/Main1.java
@@ -0,0 +1,19 @@
+package com.ecommerce;
+public class Main1 {
+	public class Main {
+	    public static void main(String[] args) {
+	        Product[] products = {
+	            new Product(1, "Laptop", "Electronics"),
+	            new Product(2, "Shirt", "Clothing"),
+	            new Product(3, "Book", "Education"),
+	            new Product(4, "Smartphone", "Electronics"),
+	            new Product(5, "Shoes", "Footwear")
+	        };
+
+	        String searchTerm = "Smartphone";
+	        Product resultLinear = SearchUtil.linearSearch(products, searchTerm);
+	        System.out.println("Linear Search Result: " + (resultLinear != null ? resultLinear : "Not Found"));
+	        Product resultBinary = SearchUtil.binarySearch(products, searchTerm);
+	        System.out.println("Binary Search Result: " + (resultBinary != null ? resultBinary : "Not Found"));
+	    }
+	}
