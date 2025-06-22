cac3bbea347727ef491b006cc06b34dfed7561ac
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 17:59:43 +0530

    Delete Week1_Design principles & Patterns/Exercise 2: Implementing Factory Method Pattern/EmailNotification.java

--- Week1_Design principles & Patterns/Exercise 2: Implementing Factory Method Pattern/EmailNotification.java
+++
@@ -1,7 +0,0 @@
-package com.notification;
-public class EmailNotification implements Notification {
-    @Override
-    public void notifyUser() {
-        System.out.println("Sending an Email Notification");
-    }
-}
