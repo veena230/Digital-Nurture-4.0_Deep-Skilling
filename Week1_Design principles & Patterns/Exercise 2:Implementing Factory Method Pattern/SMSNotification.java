a521280013ed9924c90a973d1c9430b3b04742ef
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 17:50:55 +0530

    Create SMSNotification.java

---
+++ Week1_Design principles & Patterns/Exercise 2:Implementing Factory Method Pattern/SMSNotification.java
@@ -0,0 +1,8 @@
+package com.notification;
+public class SMSNotification implements Notification {
+	 @Override
+	    public void notifyUser() {
+	        System.out.println("Sending an SMS Notification");
+	    }
+	}
+
