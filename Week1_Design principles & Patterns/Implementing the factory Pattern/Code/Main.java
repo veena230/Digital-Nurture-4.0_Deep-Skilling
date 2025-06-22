ac3edd635d39c4e65fc4fb12c8b3aa0e1b539ae1
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-22 16:24:16 +0530

    Create Main.java

---
+++ Week1_Design principles & Patterns/Implementing the factory Pattern/Code/Main.java
@@ -0,0 +1,10 @@
+package com.singleton;
+public class Main {
+	public static void main(String[] args) {
+	Logger logger1 = Logger.getInstance();
+    Logger logger2 = Logger.getInstance();
+    logger1.log("Starting application...");
+    logger2.log("Application is running...");
+    System.out.println("Are both loggers the same instance? " + (logger1 == logger2));
+}
+}
