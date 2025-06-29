ab9660349f63d7f3d3b212adb47b3ad441e7df0d
veena230 <166001019+veena230@users.noreply.github.com>
2025-06-29 19:41:42 +0530

    Create a.java

---
+++ Week2_Junit_Basics Testing Exercises/Exercise 3 : Assertions in Junit/a.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAdult() {
+        return age >= 18;
+    }
+
+    public String getNickName() {
+        return null; 
+    }
+}
