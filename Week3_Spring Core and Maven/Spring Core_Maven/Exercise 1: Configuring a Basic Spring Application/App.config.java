464d3b815ecd0f31d96592e38b2bfe5c8ddfd439
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-05 21:01:13 +0530

    Create App.config.java

---
+++ Week3_Spring Core and Maven/Spring Core_Maven/Exercise 1: Configuring a Basic Spring Application/App.config.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public HelloService helloService() {
+        return new HelloService();
+    }
+}
