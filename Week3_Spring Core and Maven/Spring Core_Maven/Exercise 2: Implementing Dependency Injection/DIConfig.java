9236bf1ba2b83af202fc7852b10d2b64b67f5fa3
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-05 21:33:24 +0530

    Create DIConfig.java

---
+++ Week3_Spring Core and Maven/Spring Core_Maven/Exercise 2: Implementing Dependency Injection/DIConfig.java
@@ -0,0 +1,9 @@
+package com.example.springdi;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.springdi")
+public class DIConfig {
+}
