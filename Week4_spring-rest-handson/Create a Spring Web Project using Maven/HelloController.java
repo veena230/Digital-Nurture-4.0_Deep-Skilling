2d97494d0303efa599496bb07fdb370e3cafbcf2
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-13 12:48:13 +0530

    Create HelloController.java

---
+++ Week4_spring-rest-handson/Create a Spring Web Project using Maven/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.springrestdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
