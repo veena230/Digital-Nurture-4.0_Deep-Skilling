3dbd52499d5000c90c31923e08a5ec6bab0c7a42
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 12:58:38 +0530

    Create CourseList.js

---
+++ Week6_ReactJS-HOL/4. Hands-on in this document/CourseList.js
@@ -0,0 +1,17 @@
+// ✅ CourseList.js
+import React from 'react';
+
+function CourseList() {
+  return (
+    <div>
+      <h2>Course List</h2>
+      <ul>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Python</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CourseList;
