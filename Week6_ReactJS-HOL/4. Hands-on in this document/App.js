76f094d1c6571061765b6ca201046d68299eb7c3
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 12:59:06 +0530

    Create App.js

---
+++ Week6_ReactJS-HOL/4. Hands-on in this document/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import CourseList from './CourseList'; // Make sure path is correct
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <CourseList />
+    </div>
+  );
+}
+
+export default App;
