29ae5f3cbf10853a747170f9d87a53f9aaf4cadc
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 13:12:59 +0530

    Create App.js

---
+++ Week6_ReactJS-HOL/5.Hands-on in this document/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginStatus from './LoginStatus';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login Status Demo</h1>
+      <LoginStatus />
+    </div>
+  );
+}
+
+export default App;
