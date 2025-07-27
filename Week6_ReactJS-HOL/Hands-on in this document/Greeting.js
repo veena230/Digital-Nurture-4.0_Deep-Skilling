c96119038ffa324813d10e68c9dc9bce159698ad
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 11:15:11 +0530

    Create Greeting.js

---
+++ Week6_ReactJS-HOL/Hands-on in this document/Greeting.js
@@ -0,0 +1,13 @@
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <>
+      <h1>React App in Eclipse</h1>
+      <Greeting name="Veena" />
+      <Greeting name="Developer" />
+    </>
+  );
+}
+
+export default App;
