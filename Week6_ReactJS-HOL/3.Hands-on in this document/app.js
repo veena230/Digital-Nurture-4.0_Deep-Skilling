900d59219071c753132c2090683885168f306835
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 12:37:40 +0530

    Create app.js

---
+++ Week6_ReactJS-HOL/3.Hands-on in this document/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
