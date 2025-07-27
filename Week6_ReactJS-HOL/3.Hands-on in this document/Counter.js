6abf4e58ac7e6a9f5ecf47cd28e277d6d527abaf
veena230 <166001019+veena230@users.noreply.github.com>
2025-07-27 12:36:52 +0530

    Create Counter.js

---
+++ Week6_ReactJS-HOL/3.Hands-on in this document/Counter.js
@@ -0,0 +1,16 @@
+// Counter.js
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
