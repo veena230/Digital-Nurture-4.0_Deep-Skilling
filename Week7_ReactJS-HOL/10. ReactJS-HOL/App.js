fe22250d1333166532a4329c577cd36bf867e021
veena230 <166001019+veena230@users.noreply.github.com>
2025-08-03 19:21:43 +0530

    Create App.js

---
+++ Week7_ReactJS-HOL/10. ReactJS-HOL/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>This is the main app content.</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
